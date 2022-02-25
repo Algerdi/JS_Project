7bb346aa78afa451f7d3dc7507e711f5167bec99
Algerdi <kzlv.vldslv@gmail.com>
2022-02-25 16:56:49 +0300

    script.js modified

--- js/script.js
+++ js/script.js
@@ -1,78 +1,21 @@
 "use strict";
 
-// let number = 5;
-// const leftBorderWidth = 1;
-// number = 10;
+const numberOfFilms = prompt('How much films do you watched?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
 
+const a = prompt('Wich movie do you watched last?', ''),
+      b = prompt('How do you rate it??', ''),
+      c = prompt('Wich movie do you watched last?', ''),
+      d = prompt('How do you rate it??', '');
 
-// console.log(number);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// {
-//     let res = 10;
-//     var luck = 15;
-// }
-
-// console.log(luck);
-
-// const obj = {
-//     name: 'Fil',
-//     age: 25,
-//     isGay: true,
-// };
-
-// console.log(obj);
-// console.log(obj["name"]);
-
-// let arr = ['taks.jpg', 'laks.jpg', 1337, {}, []];
-
-// console.log(arr[4]);
-
-// ______________________________Видео 007_____________________________
-
-
-
-// const result = confirm('Are you gay?');
-// console.log(result);
-
-// const answer = +prompt('If you`r gay print yes', '19');
-// console.log(+answer + 2);
-
-// const answers = [];
-// answers[0]= prompt('Хочешь пиццы?', '');
-// answers[1] = prompt('Какую пиццу хочешь?', '');
-// answers [2] = prompt('Какого размера?', '');
-// document.write(answers);
-// console.log(typeof(answers));
-
-// ______________________________Видео 008_____________________________
-
-// const category = 'toys';
-// alert(`hello, ${category}`);
-// console.log(`http://someurl.com/${category}/5`);
-
-
-// ______________________________Видео 009_____________________________
-
-
-// let incr = 10, decr = 10;
-
-// incr++;
-// decr--;
-
-
-
-// console.log(++incr, --decr);
-
-// console.log(2*4 == '8');
-// console.log(2*4 === '8');
-
-// const isChecked = true, 
-//     isClosed = false;
-
-// console.log(isChecked && !isClosed);
-// console.log(isChecked || isClosed);
-
-
-// ______________________________Видео 010_____________________________
-
+console.log(personalMovieDB);
