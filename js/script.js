e5ddbad00263b44592381a23b5c6bf312160a513
Algerdi <kzlv.vldslv@gmail.com>
2022-02-25 15:55:04 +0300

    Firts commit

---
+++ js/script.js
@@ -0,0 +1,78 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+// number = 10;
+
+
+
+// console.log(number);
+
+// {
+//     let res = 10;
+//     var luck = 15;
+// }
+
+// console.log(luck);
+
+// const obj = {
+//     name: 'Fil',
+//     age: 25,
+//     isGay: true,
+// };
+
+// console.log(obj);
+// console.log(obj["name"]);
+
+// let arr = ['taks.jpg', 'laks.jpg', 1337, {}, []];
+
+// console.log(arr[4]);
+
+// ______________________________Видео 007_____________________________
+
+
+
+// const result = confirm('Are you gay?');
+// console.log(result);
+
+// const answer = +prompt('If you`r gay print yes', '19');
+// console.log(+answer + 2);
+
+// const answers = [];
+// answers[0]= prompt('Хочешь пиццы?', '');
+// answers[1] = prompt('Какую пиццу хочешь?', '');
+// answers [2] = prompt('Какого размера?', '');
+// document.write(answers);
+// console.log(typeof(answers));
+
+// ______________________________Видео 008_____________________________
+
+// const category = 'toys';
+// alert(`hello, ${category}`);
+// console.log(`http://someurl.com/${category}/5`);
+
+
+// ______________________________Видео 009_____________________________
+
+
+// let incr = 10, decr = 10;
+
+// incr++;
+// decr--;
+
+
+
+// console.log(++incr, --decr);
+
+// console.log(2*4 == '8');
+// console.log(2*4 === '8');
+
+// const isChecked = true, 
+//     isClosed = false;
+
+// console.log(isChecked && !isClosed);
+// console.log(isChecked || isClosed);
+
+
+// ______________________________Видео 010_____________________________
+
